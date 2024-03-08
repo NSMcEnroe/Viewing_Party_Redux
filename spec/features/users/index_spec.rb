8cdee0dfbf1142c9bf79a69e98f9180635481ea5
Nicholas McEnroe <nsmcenro@gmail.com>
2024-03-08 10:29:49 -0700

    Test: Add initial tests for root page

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -1,9 +1,41 @@
 require "rails_helper"
 
 RSpec.describe "User Index Page", type: :feature do
-  # xit "displays the title of the application" do
-  #   visit "/"
+  xit "displays the title of the application" do
+    visit "/"
 
-  #   expect(page).to have_content("Viewing Party Redux")
-  # end
+    expect(page).to have_content("Viewing Party Redux")
+  end
+
+  xit "displays a button to create a new user" do
+    visit "/"
+
+    expect(page).to have_button("Create New User")
+  end
+
+  xit "sends the user to the correct page after clicking the new user button" do
+    visit "/"
+
+    click_on("Create New User")
+
+    expect(current_path).to eq("/register")
+  end
+
+  xit "displays a list of existing users which links to the users dashboard" do
+    user_1 = create(:user)
+    user_2 = create(:user)
+    user_3 = create(:user)
+
+    visit "/"
+
+    expect(page).to have_link(user_1.name, href: "/users/#{user_1.id}")
+    expect(page).to have_link(user_2.name, href: "/users/#{user_2.id}")
+    expect(page).to have_link(user_3.name, href: "/users/#{user_3.id}")
+  end
+
+  xit "displays a link to go back to the landing page" do
+    visit "/"
+
+    expect(page).to have_link("All Users", href: "/")
+  end
 end

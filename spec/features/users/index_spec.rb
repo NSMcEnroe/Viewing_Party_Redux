22451b7929d3add5c4c70f12752149b6e1dc13f6
Nicholas McEnroe <nsmcenro@gmail.com>
2024-03-08 10:43:25 -0700

    Feat: Add root page with proper routes and headings

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "User Index Page", type: :feature do
-  xit "displays the title of the application" do
+  it "displays the title of the application" do
     visit "/"
 
     expect(page).to have_content("Viewing Party Redux")
